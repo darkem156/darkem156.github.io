d7dc8f7245182b8c80164d4301ea728692da0290
Darkem <emmanuelchan809@gmail.com>
2022-10-30 13:55:55 -0500

    Changed description

--- frontend/src/app/components/Home.jsx
+++ frontend/src/app/components/Home.jsx
@@ -23,12 +23,12 @@ class Home extends Component
               <h1 id="title">
                 Hi. My name is Emmanuel, but you can call me Darkem.
               </h1>
-             <h3 id="subtitle">Full-Stack JR Developer</h3>
+             <h3 id="subtitle">JR Web Developer</h3>
             </div>
            <img id="me" width="100%" src="./assets/yo.png" />
           </div>
           <p id="description">
-            I am a Mexican and Student Full-Stack Developer. I am 17-years-old and I have experiencie with Personal Projects in the Node JS Environment
+            I am a Mexican Student and Web Developer. I am 17-years-old and I have experiencie with Personal Projects in the Node JS Environment
           </p>
           <div id="cv-container"><a href="../assets/cv.pdf">Resume</a></div>
         </div>

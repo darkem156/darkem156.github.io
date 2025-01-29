c709649ceea5eb18ac0eba76ac4939280299ddaa
Emmanuel <emmanuelchan809@gmail.com>
2025-01-29 18:36:33 -0500

    New portfolio

--- frontend/src/app/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import App from './components/App.jsx';
-
-const app = document.getElementById("app");
-const root = ReactDOM.createRoot(app);
-root.render(<App />);
